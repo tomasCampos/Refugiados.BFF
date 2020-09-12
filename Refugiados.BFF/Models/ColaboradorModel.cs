d47a246d01df49acc231c9164b0f69581872e02c
tomas <tomasantoniocampos@gmail.com>
2020-09-12 00:59:50 -0300

    Implementação da camada de repositório

---
+++ Refugiados.BFF/Models/ColaboradorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Refugiados.BFF.Models
+{
+    public class ColaboradorModel
+    {
+        public int CodigoColaborador { get; set; }
+        public string NomeColaborador { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public int CodigoUsuario { get; set; }
+    }
+}
---
+++ Repositorio/Repositorios/ColaboradorRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using Repositorio.Dtos;
+using Repositorio.Insfraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositorio.Repositorios
+{
+    public class ColaboradorRepositorio : IColaboradorRepositorio
+    {
+        private readonly DataBaseConnector _dataBase;
+
+        #region QUERIES 
+
+        private const string OBTER_COLABORADOR_POR_CODIGO_USUARIO_SQL = @"SELECT * FROM heroku_93ac2d8811d872a.colaborador
+                                                                   WHERE codigo_usuario = @codigo_usuario;";
+
+        private const string OBTER_COLABORADOR_SQL = @"SELECT * FROM heroku_93ac2d8811d872a.colaborador
+                                                        WHERE codigo_colaborador = @codigo_colaborador;";
+
+        private const string CADASTRAR_COLABORADOR_SQL = @"INSERT INTO `heroku_93ac2d8811d872a`.`colaborador`
+                                                    (`codigo_colaborador`,
+                                                    `nome_colaborador`,
+                                                    `data_criacao`,
+                                                    `data_alteracao`,
+                                                    `codigo_usuario`)
+                                                    VALUES
+                                                    (default,
+                                                    @nome_colaborador,
+                                                    default,
+                                                    CURRENT_TIMESTAMP,
+                                                    @codigo_usuario);";
+
+        #endregion
+
+        public void AtualizarColaborador(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarColaborador(string nome, int codigoUsuario)
+        {
+            _dataBase.Executar(CADASTRAR_COLABORADOR_SQL, new { nome_colaborador = nome, codigo_usuario = codigoUsuario });
+        }
+
+        public ColaboradorDto ObterColaboradorPorCodigo(int? codigo)
+        {
+            var colaborador = _dataBase.Selecionar<ColaboradorDto>(OBTER_COLABORADOR_SQL, new { codigo_colaborador = codigo }).FirstOrDefault();
+            return colaborador;
+        }
+
+        public ColaboradorDto ObterColaboradorPorCodigoUsuario(int? codigoUsuario)
+        {
+            var colaborador = _dataBase.Selecionar<ColaboradorDto>(OBTER_COLABORADOR_POR_CODIGO_USUARIO_SQL, new { codigo_usuario = codigoUsuario }).FirstOrDefault();
+            return colaborador;
+        }
+    }
+
+    public interface IColaboradorRepositorio
+    {
+        void CadastrarColaborador(string nome, int codigoUsuario);
+        ColaboradorDto ObterColaboradorPorCodigo(int? codigo);
+        ColaboradorDto ObterColaboradorPorCodigoUsuario(int? codigoUsuario);
+        void AtualizarColaborador(string nome);
+    }
+}
